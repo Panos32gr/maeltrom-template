9cc4e967e60edabd70d8452277a32de695492b72
Ambraxis <Namkorr@gmail.com>
2024-08-06 16:36:56 +0300

    text animations with gsap

--- script.js
+++ script.js
@@ -1,89 +1,76 @@
 
-
-
-
 var bannerStatus = 1;
 var bannerTimer = 4000;
 
-window.onload = function() {
-    bannerLoop()
+window.onload = function () {
+  bannerLoop();
 };
 
-
-var bannerStart = setInterval(function() {
-    bannerLoop()
+var bannerStart = setInterval(function () {
+  bannerLoop();
 }, bannerTimer);
 
 function bannerLoop() {
+  if (bannerStatus === 1) {
+    document.getElementById("imgban2").style.opacity = "0";
 
-    
+    setTimeout(function () {
+      document.getElementById("imgban1").style.right = "0px";
+      document.getElementById("imgban1").style.zIndex = "1000";
+      document.getElementById("imgban2").style.right = "-1200px";
+      document.getElementById("imgban2").style.zIndex = "1500";
+      document.getElementById("imgban3").style.right = "1200px";
+      document.getElementById("imgban3").style.zIndex = "500";
+    }, 500);
 
-    if (bannerStatus === 1) {
+    setTimeout(function () {
+      document.getElementById("imgban2").style.opacity = "1";
+    }, 1000);
 
-        document.getElementById("imgban2").style.opacity = "0";
+    bannerStatus = 2;
+  } else if (bannerStatus === 2) {
+    document.getElementById("imgban3").style.opacity = "0";
 
-        setTimeout(function(){
-            document.getElementById("imgban1").style.right = "0px";
-            document.getElementById("imgban1").style.zIndex = "1000";
-            document.getElementById("imgban2").style.right = "-1200px";
-            document.getElementById("imgban2").style.zIndex = "1500";
-            document.getElementById("imgban3").style.right = "1200px";
-            document.getElementById("imgban3").style.zIndex = "500";
-        }, 500);
+    setTimeout(function () {
+      document.getElementById("imgban2").style.right = "0px";
+      document.getElementById("imgban2").style.zIndex = "1000";
+      document.getElementById("imgban3").style.right = "-1200px";
+      document.getElementById("imgban3").style.zIndex = "1500";
+      document.getElementById("imgban1").style.right = "1200px";
+      document.getElementById("imgban1").style.zIndex = "500";
+    }, 500);
 
-        setTimeout(function(){
-            document.getElementById("imgban2").style.opacity = "1";
-        }, 1000);
+    setTimeout(function () {
+      document.getElementById("imgban3").style.opacity = "1";
+    }, 1000);
 
-        bannerStatus = 2;
-    }
+    bannerStatus = 3;
+  } else if (bannerStatus === 3) {
+    document.getElementById("imgban1").style.opacity = "0";
 
+    setTimeout(function () {
+      document.getElementById("imgban3").style.right = "0px";
+      document.getElementById("imgban3").style.zIndex = "1000";
+      document.getElementById("imgban1").style.right = "-1200px";
+      document.getElementById("imgban1").style.zIndex = "1500";
+      document.getElementById("imgban2").style.right = "2000px";
+      document.getElementById("imgban2").style.zIndex = "500";
+    }, 500);
 
-    else if (bannerStatus === 2) {
+    setTimeout(function () {
+      document.getElementById("imgban1").style.opacity = "1";
+    }, 1000);
 
-        document.getElementById("imgban3").style.opacity = "0";
-
-        setTimeout(function(){
-            document.getElementById("imgban2").style.right = "0px";
-            document.getElementById("imgban2").style.zIndex = "1000";
-            document.getElementById("imgban3").style.right = "-1200px";
-            document.getElementById("imgban3").style.zIndex = "1500";
-            document.getElementById("imgban1").style.right = "1200px";
-            document.getElementById("imgban1").style.zIndex = "500";
-        }, 500);
-
-        setTimeout(function(){
-            document.getElementById("imgban3").style.opacity = "1";
-        }, 1000);
-
-        bannerStatus = 3;
-    }
+    bannerStatus = 1;
+  }
+}
 
 
-    else if (bannerStatus === 3) {
-
-        document.getElementById("imgban1").style.opacity = "0";
-
-        setTimeout(function(){
-            document.getElementById("imgban3").style.right = "0px";
-            document.getElementById("imgban3").style.zIndex = "1000";
-            document.getElementById("imgban1").style.right = "-1200px";
-            document.getElementById("imgban1").style.zIndex = "1500";
-            document.getElementById("imgban2").style.right = "2000px";
-            document.getElementById("imgban2").style.zIndex = "500";
-        }, 500);
-
-        setTimeout(function(){
-            document.getElementById("imgban1").style.opacity = "1";
-        }, 1000);
-
-        bannerStatus = 1;
-    }
-
-};
+gsap.registerPlugin("scrollTrigger");
 
 
 
-gsap.from(".vfxinfo", {duration: 2, right: "-100%", ease: "power1"})
+gsap.from(".vfxinfo", {scrollTrigger: {trigger:".vfxinfo", start: "center center", markers:true,toggleActions: "play none none none"}, duration: 1, right: "-100%", ease: "power1.in"});
 
-gsap.from(".gamesinfo", {duration: 2, left: "-100%", ease: "power1"})     
+
+gsap.from(".gamesinfo", {scrollTrigger: {trigger:".gamesinfo", start: "center center", markers:true,toggleActions: "play none none none"}, duration: 2, left: "-100%", ease: "power1"});
