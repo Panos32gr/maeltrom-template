b276b7ccec44f1310739280d2ca7f35cd8e14e60
ΑΓΓΗΣ ΠΑΝΑΓΙΩΤΗΣ <Namkorr@gmail.com>
2024-09-10 21:38:28 +0300

    fixed

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function bannerLoop() {
 }
 
 function reverseBanner() {
-  if (bannerStatus === 2) {
+  if (bannerStatus === 1) {
     document.getElementById("imgban1").style.opacity = "0";
     setTimeout(function () {
       document.getElementById("imgban1").style.right = "-1200px";
@@ -101,11 +101,11 @@ function reverseBanner() {
       document.getElementById("imgban2").style.zIndex = "-2";
     }, 500);
     setTimeout(function () {
-      document.getElementById("imgban1").style.opacity = "1";
+      document.getElementById("imgban3").style.opacity = "1";
     }, 1000);
 
     bannerStatus = 3;
-  } else if (bannerStatus === 3) {
+  } else if (bannerStatus === 2) {
     document.getElementById("imgban2").style.opacity = "0";
     setTimeout(function () {
       document.getElementById("imgban2").style.right = "-1200px";
@@ -116,11 +116,11 @@ function reverseBanner() {
       document.getElementById("imgban3").style.zIndex = "-2";
     }, 500);
     setTimeout(function () {
-      document.getElementById("imgban2").style.opacity = "1";
+      document.getElementById("imgban1").style.opacity = "1";
     }, 1000);
 
     bannerStatus = 1;
-  } else if (bannerStatus === 1) {
+  } else if (bannerStatus === 3) {
     document.getElementById("imgban3").style.opacity = "0";
     setTimeout(function () {
       document.getElementById("imgban3").style.right = "-1200px";
@@ -131,7 +131,7 @@ function reverseBanner() {
       document.getElementById("imgban1").style.zIndex = "-2";
     }, 500);
     setTimeout(function () {
-      document.getElementById("imgban3").style.opacity = "1";
+      document.getElementById("imgban2").style.opacity = "1";
     }, 1000);
 
     bannerStatus = 2;
